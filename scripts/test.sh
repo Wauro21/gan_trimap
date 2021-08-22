5b06e8bedf0160008d10132f1ce5d0524d240db1
Mauricio <maurio.aravena@sansano.usm.cl>
2021-08-22 10:37:30 -0400

    Scripts for training and testing

---
+++ scripts/test.sh
@@ -0,0 +1,1 @@
+python3 test.py --name w --checkpoints_dir ./checkpoints --batchSize 2 --loadSize 256 --fineSize 128 --label_nc 0  --dataroot datasets/w/ --nThreads 8 --no_instance --save_latest_freq 10 --save_epoch_freq 10 --which_epoch 200 --results_dir ./results
